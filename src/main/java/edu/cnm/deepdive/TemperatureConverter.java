594fa2e09a8acd40e167ed28a6732593306dc634
nmitchell313 <nmitchel11@cnm.edu>
2021-09-28 14:57:02 -0600

    Implemented F2C conversion and read from command line.

--- src/main/java/edu/cnm/deepdive/TemperatureConverter.java
+++ src/main/java/edu/cnm/deepdive/TemperatureConverter.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 
 public class TemperatureConverter {
 
+    private static final double CELSIUS_FAHRENHEIT_SCALE_FACTOR = 1.8;
+    private static final int CELSIUS_FAHRENHEIT_SCALE_OFFSET = 32;
+
     public static void main(String[] args) {
+/*
         Scanner scanner = new Scanner(System.in);
         while (true) {
           System.out.println("Please enter a temperature in degrees celsius");
@@ -12,10 +16,19 @@ public class TemperatureConverter {
           double fahrenheit = convertC2F(celsius);
           System.out.println(fahrenheit);
         }
+*/
+        for (String arg : args) {
+            double f = Double.parseDouble(arg);
+            System.out.println(convertF2C(f));
+        }
     }
 
     public static double convertC2F(double celsius) {
-        return 1.8 * celsius + 32; // C = (F - 32) / 1.8
+        return CELSIUS_FAHRENHEIT_SCALE_FACTOR * celsius + CELSIUS_FAHRENHEIT_SCALE_OFFSET;
+    }
+
+    public static double convertF2C(double fahrenheit) {
+        return (fahrenheit - CELSIUS_FAHRENHEIT_SCALE_OFFSET) / CELSIUS_FAHRENHEIT_SCALE_FACTOR;
     }
 
 }
